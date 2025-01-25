21280165bd0d7c686c1160cf0f257a4fd377860e
Yusuf Fatih Kılınç <yusuffatihk1939@gmail.com>
2025-01-25 19:15:01 +0300

    simple tcp and udp server

---
+++ README.md
@@ -0,0 +1,18 @@
+# C Network Programming - TCP/UDP Server-Client
+
+This repository contains simple implementations of TCP and UDP server-client applications written in C. These programs demonstrate basic network communication concepts and are based on the concepts learned from **Beej's Guide to Network Programming in C**.
+
+## Project Structure
+
+- **notes.c**: A file containing some personal notes and learning from the guide.
+- **tcp-server**: Contains a TCP server and client implementation.
+- **udp-server**: Contains a UDP server and client implementation.
+
+## How to Run
+
+1. Navigate to the `tcp-server` or `udp-server` directory.
+2. Compile the server and client programs:
+   ```bash
+   gcc server.c -o server
+   gcc client.c -o client
+
---
+++ notes.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+
+
+int main() {
+   
+    // hmmm 
+    // handshake 
+
+    /*    
+    struct sockaddr_in {
+        hort int sin_family; // Address family, AF_INET (tcp or udp)
+        unsigned short int sin_port; // Port number (max 65,535)
+        struct in_addr sin_addr; // Internet address 
+        unsigned char sin_zero[8]; // Same size as struct sockaddr
+    };
+
+
+
+    struct sockaddr {
+        unsigned short sa_family; // address family, AF_xxx
+        char sa_data[14]; // 14 bytes of protocol address
+    }
+
+
+    struct in_addr {
+        uint32_t s_addr; // unsigned 4 byte
+    };
+
+
+     */
+    
+
+
+    return 13; // :) 
+}
